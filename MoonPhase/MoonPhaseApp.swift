b117553bfd70d1ae30054a9c4cbb0bc4c0bf4da2
Bhavadarshini <bhavadarshini05@gmail.com>
2025-06-20 13:10:15 +0530

    Initial Commit

---
+++ MoonPhase/MoonPhaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonPhaseApp.swift
+//  MoonPhase
+//
+//  Created by Bhavadarshini on 20/06/25.
+//
+
+import SwiftUI
+
+@main
+struct MoonPhaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
